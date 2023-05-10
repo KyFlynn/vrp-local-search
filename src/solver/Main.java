4faa3c9dfcf7d2aa833dc6c1829cb9918fe1424e
Kyran Flynn <kflynn6@cs.brown.edu>
2023-05-10 17:17:40 -0400

    Fixed IP model for complete solution, works on instance 16_

--- src/solver/Main.java
+++ src/solver/Main.java
@@ -1,5 +1,6 @@
 package solver;
 
+import solver.complete.IPModel;
 import solver.incomplete.BestImprovement;
 import solver.util.Timer;
 
@@ -18,18 +19,29 @@ public class Main {
         String filename = path.getFileName().toString();
         System.out.println("Instance: " + input);
 
-        VRPInstance instance = new VRPInstance(input);
         Timer watch = new Timer();
+        VRPInstance instance = new VRPInstance(input);
+        double objVal = -1.0;
+        String solution = "";
         watch.start();
-        BestImprovement solver = new BestImprovement(instance, 180);
-        double result = solver.solve();
+        // Complete algorithm if numCustomers low enough
+        if (instance.numCustomers < 30) {
+            IPModel ipModel = new IPModel(instance);
+            objVal = ipModel.solve();
+            solution += ipModel.solutionToString();
+            ipModel.solutionToFile(filename);
+        } else {
+            BestImprovement solver = new BestImprovement(instance, 5);
+            objVal = solver.solve();
+            solution += solver.bestSolutionToString();
+            solver.bestSolutionToFile(filename);
+        }
         watch.stop();
 
         System.out.println("{\"Instance\": \"" + filename +
                 "\", \"Time\": " + String.format("%.2f",watch.getTime()) +
-                ", \"Result\": " + String.format("%.2f", result) +
-                ", \"Solution\": \"" + solver.bestSolutionToString() +  "\"}");
+                ", \"Result\": " + String.format("%.2f", objVal) +
+                ", \"Solution\": \"" + solution +  "\"}");
 
-        solver.bestSolutionToFile(filename);
     }
 }
--- src/solver/complete/IPModel.java
+++ src/solver/complete/IPModel.java
@@ -1,6 +1,5 @@
 package solver.complete;
 
-import solver.util.Timer;
 import solver.VRPInstance;
 
 import ilog.concert.IloException;
@@ -8,10 +7,12 @@ import ilog.concert.IloNumExpr;
 import ilog.concert.IloNumVar;
 import ilog.concert.IloNumVarType;
 import ilog.cplex.IloCplex;
+import solver.util.Node;
+import solver.util.Route;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.PrintStream;
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,7 +40,7 @@ public class IPModel {
         for (int v = 0; v < vrp.numVehicles; v++) {
             for (int i = 0; i < vrp.numCustomers; i++) {
                 for (int j = 0; j < vrp.numCustomers; j++) {
-                    vehicleArcChoice[v][i][j] = cp.numVar(0, 1, IloNumVarType.Int);
+                    vehicleArcChoice[v][i][j] = cp.numVar(0, 1, IloNumVarType.Int, String.format("V%dC%d,%d", v, i, j));
                 }
             }
         }
@@ -51,27 +52,20 @@ public class IPModel {
             }
         }
 
-        // Edges are undirected
-        // for (int i = 0; i < vrp.numCustomers; i++) {
-        //     for (int j = i+1; j < vrp.numCustomers; j++) {
-        //         for (int v = 0; v < vrp.numVehicles; v++) {
-        //             cp.addEq(vehicleArcChoice[v][i][j], vehicleArcChoice[v][j][i]);
-        //         }
-        //     }
-        // }
-
-        // Each edge should only be taken by one vehicle
+        // Each edge (undirected) should only be taken by one vehicle
         for (int i = 0; i < vrp.numCustomers; i++) {
             for (int j = i+1; j < vrp.numCustomers; j++) {
-                IloNumExpr[] vehicleSlice = new IloNumExpr[vrp.numVehicles];
+                IloNumExpr[] vehicleSlice = new IloNumExpr[2 * vrp.numVehicles];
                 for (int v = 0; v < vrp.numVehicles; v++) {
-                    vehicleSlice[v] = vehicleArcChoice[v][i][j];
+                    // Both directions
+                    vehicleSlice[2 * v] = vehicleArcChoice[v][i][j];
+                    vehicleSlice[2 * v + 1] = vehicleArcChoice[v][j][i];
                 }
                 cp.addLe(cp.sum(vehicleSlice), 1);
             }
         }
 
-        // Each customer is served (exit)
+        // Each customer is served (entry)
         for (int i = 0; i < vrp.numCustomers; i++) {
             IloNumExpr[] vehicleCustomerSlice = new IloNumExpr[vrp.numVehicles * vrp.numCustomers];
             for (int v = 0; v < vrp.numVehicles; v++) {
@@ -86,7 +80,7 @@ public class IPModel {
             }
         }
 
-        // Each customer is served (entry)
+        // Each customer is served (exit)
         for (int j = 0; j < vrp.numCustomers; j++) {
             IloNumExpr[] vehicleCustomerSlice = new IloNumExpr[vrp.numVehicles * vrp.numCustomers];
             for (int v = 0; v < vrp.numVehicles; v++) {
@@ -147,8 +141,8 @@ public class IPModel {
         }
         cp.addMinimize(cp.sum(totalDistance));
 
-        // Save model to file for visual check -> ".Lp" extension required
-        cp.exportModel("Model.Lp");
+//        // Save model to file for visual check -> ".Lp" extension required
+//        cp.exportModel("Model.Lp");
     }
 
     // Get the vehicle route information into a nice adjacency chart.
@@ -173,12 +167,9 @@ public class IPModel {
             boolean subtoursExist = false;
             int[][][] M = getVariableValues();
 //            System.out.println(Arrays.deepToString(M));
+            // 1) DFS starting at the depot to see which customers are in a valid tour.
             boolean[] visited = new boolean[vrp.numCustomers];
             for (int v = 0; v < vrp.numVehicles; v++) {
-//                System.out.println(String.format("Analyzing Vehicle: %d", v));
-
-                // 1) DFS starting at the depot to see which customers are in a valid tour.
-
                 // Keep track of the tour for printing.
                 ArrayList<Integer> tour = new ArrayList<>();
                 // Start at depot.
@@ -214,36 +205,37 @@ public class IPModel {
                             }
                         }
                         if (!found) {
-//                            System.out.println(Arrays.deepToString(M));
+                            // System.out.println(Arrays.deepToString(M));
                             throw new Exception("Path is not a tour, model was implemented incorrectly.");
                         }
                     }
                 }
-//                System.out.println("TOUR FOUND:");
-//                System.out.println(tour);
+//                System.out.println("Tour found:");
+//                for (int i = 0; i < tour.size(); i++) {
+//                    System.out.print(String.format("%d ", tour.get(i)));
+//                }
+//                System.out.println("");
+            }
 
-                // 2) DFS through all other arcs for this vehicle to see which customers are in a subtour.
-
+            // 2) DFS through all other arcs for this vehicle to see which customers are in a subtour.
+            for (int v = 0; v < vrp.numVehicles; v++) {
                 for (int i = 1; i < vrp.numCustomers; i++) {
                     // Customer already included in a discovered tour/subtour
                     if (visited[i]) continue;
                     for (int j = 1; j < vrp.numCustomers; j++) {
                         // Customer already included in a discovered tour/subtour
                         if (visited[j]) continue;
-
                         // Subtour arc found
                         if (M[v][i][j] == 1) {
                             // Keep track of this subtour
                             ArrayList<Integer> subtourCustomers = new ArrayList<>();
-                            ArrayList<IloNumExpr> subtour = new ArrayList<>();
                             // Source is i
                             int src = i;
                             // Mark source as visited and add to subtour.
                             visited[src] = true;
                             subtourCustomers.add(src);
                             // Travel through the first arc found, then DFS.
-                            curr = j;
-                            subtour.add(vehicleArcChoice[v][src][curr]);
+                            int curr = j;
                             while (true) {
                                 // Mark as visited.
                                 visited[curr] = true;
@@ -251,7 +243,6 @@ public class IPModel {
 
                                 // Check if we go to the source of the subtour next; if so we are done.
                                 if (M[v][curr][src] == 1) {
-                                    subtour.add(vehicleArcChoice[v][curr][src]);
                                     break;
                                 }
 
@@ -260,37 +251,97 @@ public class IPModel {
                                 for (int dest = 1; dest < vrp.numCustomers; dest++) {
                                     if (M[v][curr][dest] == 1) {
                                         curr = dest;
-                                        subtour.add(vehicleArcChoice[v][curr][dest]);
                                         found = true;
                                         break;
                                     }
                                 }
                                 if (!found) {
-//                                    System.out.println(Arrays.deepToString(M));
                                     throw new Exception("Path is not a subtour, model was implemented incorrectly.");
                                 }
                             }
-                            // We need to enforce that all of these edges are never taken for any vehicle.
-                            if (subtour.size() > 0) {
-//                                System.out.println("SUBTOUR FOUND:");
-//                                System.out.println(subtourCustomers);
-                                cp.addLe(cp.sum(subtour.toArray(new IloNumExpr[subtour.size()])), subtour.size() - 1);
+                            // We need to enforce that all of these subtours are never taken for any vehicle.
+                            if (subtourCustomers.size() > 0) {
+                                System.out.println("Subtour found:");
+                                for (int c = 0; c < subtourCustomers.size(); c++) {
+                                    System.out.print(String.format("%d ", subtourCustomers.get(c)));
+                                }
+                                System.out.println("");
+                                ArrayList<IloNumExpr> subtourEdges = new ArrayList<>();
+                                for (int veh = 0; veh < vrp.numVehicles; veh++) {
+                                    subtourEdges.add(
+                                            vehicleArcChoice[veh][subtourCustomers.get(subtourCustomers.size()-1)][subtourCustomers.get(0)]);
+                                    subtourEdges.add(
+                                            vehicleArcChoice[veh][subtourCustomers.get(0)][subtourCustomers.get(subtourCustomers.size()-1)]);
+                                    for (int c = 1; c < subtourCustomers.size(); c++) {
+                                        subtourEdges.add(
+                                                vehicleArcChoice[veh][subtourCustomers.get(c-1)][subtourCustomers.get(c)]);
+                                        subtourEdges.add(
+                                                vehicleArcChoice[veh][subtourCustomers.get(c)][subtourCustomers.get(c-1)]);
+                                    }
+                                }
+                                cp.addLe(
+                                    cp.sum(
+                                        subtourEdges.toArray(new IloNumExpr[subtourEdges.size()])),
+                                        (subtourEdges.size() / (2 * vrp.numVehicles)) - 1);
                                 subtoursExist = true;
                             }
                         }
                     }
                 }
             }
-            if (!subtoursExist) return cp.getObjValue();
+            if (!subtoursExist) {
+                return cp.getObjValue();
+            }
         }
     }
 
     //  Extracts the solution into the desired format
-    public String printSolution() throws IloException {
-        // Print out the objective value and whether it was optimal
-        String s = String.format("%.2f %d\n", cp.getObjValue(), feasible ? 1 : 0);
-
+    public String solutionToString() throws IloException {
+        String s = "";
         // Go through each vehicle and see what its tour was
+        int[][][] M = getVariableValues();
+        for (int v = 0; v < vrp.numVehicles; v++) {
+            String row = "0 ";
+            int curr = 0;
+            // Check if an exit exists from the depot.
+            for (int i = 1; i < vrp.numCustomers; i++) {
+                if (M[v][curr][i] == 1) {
+                    curr = i;
+                    break;
+                }
+            }
+            // If a non-depot exit exists
+            if (curr > 0) {
+                row += String.format("%d ", curr);
+                // Until we get back to the depot...
+                while (true) {
+                    // Check if we can go to the depot; if so, we are done.
+                    if (M[v][curr][0] == 1) {
+                        row += "0";
+                        break;
+                    }
+
+                    // Otherwise, go through the other locations and see what our next hop is.
+                    for (int dest = 1; dest < vrp.numCustomers; dest++) {
+                        if (M[v][curr][dest] == 1) {
+                            curr = dest;
+                            row += String.format("%d ", dest);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                // Note: a case where no arc is chosen for the vehicle exists, so default to arc 0 - 0
+                row += "0 ";
+            }
+            // Once we are back to the depot, add this row and do the next vehicle.
+            s += row;
+        }
+        return s;
+    }
+
+    public void solutionToFile(String filename) throws FileNotFoundException, IloException {
+        String s = String.format("%.2f %d\n", cp.getObjValue(), feasible ? 1 : 0);
         int[][][] M = getVariableValues();
         for (int v = 0; v < vrp.numVehicles; v++) {
             String row = "0 ";
@@ -326,10 +377,12 @@ public class IPModel {
                 // Note: a case where no arc is chosen for the vehicle exists, so default to arc 0 - 0
                 row += "0";
             }
-
             // Once we are back to the depot, add this row and do the next vehicle.
             s += row + "\n";
         }
-        return s;
+        PrintStream out = new PrintStream(new FileOutputStream("input/" + filename + ".sol"));
+        out.print(s);
+        out.flush();
+        out.close();
     }
 }
